5b3d62bd6b0c71105095e5071e381f0aa5c8ae0a
TinaTiel <contact@tinatiel.com>
2021-06-26 10:55:55 +0200

    Updated python file so I can suppress stderr to get better log info

--- verify_conn.py
+++ verify_conn.py
@@ -1,7 +1,17 @@
 # pip install websocket-client
+import sys
 import websocket
 
-ws = websocket.WebSocket()
-ws.connect("ws://localhost:4444", timeout=3)
-print(ws.recv())
-ws.close(timeout=3)
+nostderr = len(sys.argv) >= 2 and sys.argv[1] == "suppress_std_err"
+print("Suppressing stderr: " + str(nostderr))
+
+try:
+    ws = websocket.WebSocket()
+    ws.connect("ws://localhost:4444", timeout=3)
+    print(ws.recv())
+    ws.close(timeout=3)
+except Exception as exception:
+    if(nostderr):
+        print(exception)
+    else:
+        raise exception
