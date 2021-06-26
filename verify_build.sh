0a241f684dc496e2b419c63ca075fe6fde126dc2
TinaTiel <contact@tinatiel.com>
2021-06-26 11:45:24 +0200

    Added wait and explicitly printing all logs, they seem incomplete

--- verify_build.sh
+++ verify_build.sh
@@ -9,6 +9,9 @@ pip install websocket-client
 echo "Running the image"
 docker run -d -p 4444:4444 $1:latest
 
+# Wait a few seconds
+sleep 3
+
 # Verify the conn, but suppress stderr so we can collect logs
 echo "Attempting connect:"
 python ./verify_conn.py suppress_std_err
@@ -16,7 +19,7 @@ python ./verify_conn.py suppress_std_err
 # Collect the logs
 CONTAINER_ID=$(docker ps --filter="ancestor=$1" --latest --format="{{.ID}}")
 echo "Collecting Logs for container ID ${CONTAINER_ID}"
-docker logs ${CONTAINER_ID}
+docker logs -n all ${CONTAINER_ID}
 
 # Run again; if it failed, then it will stderr and fail the build here
 python ./verify_conn.py
